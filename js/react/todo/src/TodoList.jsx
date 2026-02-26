11344ffc6f195aad2023e02bb09426080dc861e8
Csrand <chrystiansenna@gmail.com>
2026-02-26 16:16:18 -0300

    feat: revisei o basico do React.
    fiz um app todo

---
+++ js/react/todo/src/TodoList.jsx
@@ -0,0 +1,18 @@
+import { TodoItem } from "./TodoItems"
+
+export function TodoList({todos, toggleTodo, deleteTodo}) {
+    return (
+        <ul className="list">
+            {todos.map(todo => {
+                return (
+                    <TodoItem
+                        {...todo}
+                        key={todo.id}
+                        toggleTodo={toggleTodo}
+                        deleteTodo={deleteTodo}
+                    />
+                )
+            })}
+        </ul>
+    )
+};
